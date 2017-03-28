bd9563b6cd04516ace22cf57b41a00a0b7bd0598
kittysquee <kittysquee@gmail.com>
2017-03-28 23:25:59 +0100

    Allows players names to be entered

--- app.rb
+++ app.rb
@@ -7,7 +7,17 @@ class Battle < Sinatra::Base
 
   get '/' do
     "Testing infrastructure working!"
-  end 
+  end
+
+  get '/names' do
+    erb(:index)
+  end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb(:play)
+  end
 
   run! if app_file == $0
 end
