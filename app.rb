2fbbf5a8dfa1476b24e73bbfad2280a7bffe3b66
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-29 13:58:50 +0100

    add batman and robin

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-require 'player'
+require './lib/player'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -28,6 +28,7 @@ class Battle < Sinatra::Base
   get '/attack' do
     @player_1_name = $player_1.name
     @player_2_name = $player_2.name
+    $player_1.attack($player_2)
     erb(:attack)
   end
 
